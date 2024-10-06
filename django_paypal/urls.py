81d101bb0db83b35f40859d06788ce87fa18d915
tesfahiwet trunh <eminemmerne@gmail.com>
2024-10-06 20:06:21 +0300

    first commit

---
+++ django_paypal/urls.py
@@ -0,0 +1,13 @@
+# course_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+from payment import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('courses/<int:course_id>/', views.course_detail, name='course_detail'),
+    path('courses/<int:course_id>/create-order/', views.create_order, name='create_order'),
+    path('courses/<int:course_id>/payment-success/', views.payment_success, name='payment_success'),
+    path('courses/<int:course_id>/payment-cancel/', views.payment_cancel, name='payment_cancel'),
+]
